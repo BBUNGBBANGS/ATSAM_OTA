4820e62ae2d2f04d2c912cf820493600a9da799c
Jisu.Seo <susjs90@gmail.com>
2023-04-01 22:25:18 +0900

    최초 커밋

---
+++ ATSAM_OTA/main.c
@@ -0,0 +1,21 @@
+/*
+ * ATSAM_OTA.c
+ *
+ * Created: 2023-04-01 오후 10:21:17
+ * Author : seojisu
+ */ 
+
+
+#include "sam.h"
+
+
+int main(void)
+{
+    /* Initialize the SAM system */
+    SystemInit();
+
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
